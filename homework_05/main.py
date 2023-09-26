eadbce7e2b97d1a0fdcac16786fa5a2d00253548
n.pistsov <n.pistsov@ritm.media>
2023-09-26 14:58:25 +0300

    hw5 green tests

--- homework_05/main.py
+++ homework_05/main.py
@@ -2,9 +2,7 @@ __all__ = ("app",)
 
 from app import app
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(
-        # host="localhost",
-        # port=5000,
         debug=True,
     )
