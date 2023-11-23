0755107e857ae0df53f87ab03602ee7c0089f182
n.pistsov <n.pistsov@ritm.media>
2023-11-23 23:21:29 +0300

    amended models registration method in admin.py

--- homework_07/RobinBobin/mainapp/admin.py
+++ homework_07/RobinBobin/mainapp/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import Product, Profile
 
-admin.site.register(Product)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = "pk", "name", "username", "contact"
+    list_display_links = "pk", "name", "username"
 
-admin.site.register(Profile)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = "pk", "name", "ccal"
+    list_display_links = "pk", "name"
